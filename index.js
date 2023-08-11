d2fa2c4d1e6f2d29f0da19a0f0d805b0b1ce7ad0
BuildTools <unconfigured@null.spigotmc.org>
2023-08-11 16:48:28 +1000

    eslint config changes and index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+// Require the necessary discord.js classes
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
+
+// Create a new client instance
+const client = new Client({ intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMessageReactions,
+    ] });
+
+// Initialise a prefix for the bot to see message commands
+const prefix = 'bee';
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
+
+// When the bot sees a message, it will analyse it for a prefix or if the sender is a bot.
+// If the keyword after the prefix is a word in a command it will execute that specific command.
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'ping') {
+        message.channel.send('pong!');
+    }
+    else if (command === 'amogus') {
+        message.channel.send('sussy amgus');
+    }
+});
+
+// Log in to Discord with your client's token
+client.login(token);
